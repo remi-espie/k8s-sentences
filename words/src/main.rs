826a486c16540c623709aba5c9f3ffed3fe522a3
remi <remi.espie@etu.umontpellier.fr>
2025-04-23 11:31:26 +0200

    fix: fmt

--- words/src/main.rs
+++ words/src/main.rs
@@ -1,10 +1,9 @@
-use std::env;
 use actix_web::{web, App, HttpServer, Responder};
 use rand::prelude::IndexedRandom;
-use tokio_postgres::{NoTls, Client};
+use std::env;
+use tokio_postgres::{Client, NoTls};
 
 async fn get_noun(client: web::Data<Client>) -> impl Responder {
-
     // Query the database for nouns
     let rows = client
         .query("SELECT noun FROM nouns", &[])
@@ -12,11 +11,8 @@ async fn get_noun(client: web::Data<Client>) -> impl Responder {
         .expect("Failed to execute query");
 
     // Collect the nouns into a vector
-    let nouns: Vec<String> = rows
-        .iter()
-        .map(|row| row.get("noun"))
-        .collect();
-    
+    let nouns: Vec<String> = rows.iter().map(|row| row.get("noun")).collect();
+
     let default = "default_noun".to_string();
 
     // Pick a random noun
@@ -33,10 +29,7 @@ async fn get_verb(client: web::Data<Client>) -> impl Responder {
         .expect("Failed to execute query");
 
     // Collect the words into a vector
-    let nouns: Vec<String> = rows
-        .iter()
-        .map(|row| row.get("verb"))
-        .collect();
+    let nouns: Vec<String> = rows.iter().map(|row| row.get("verb")).collect();
 
     let default = "default_verb".to_string();
 
@@ -49,8 +42,9 @@ async fn get_verb(client: web::Data<Client>) -> impl Responder {
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     // Load the database url from environment variables
-    let database_url: String  = env::var("DATABASE_URL").unwrap_or("postgres://postgres:postgres@postgres:5432/words".to_string());
-    
+    let database_url: String = env::var("DATABASE_URL")
+        .unwrap_or("postgres://postgres:postgres@postgres:5432/words".to_string());
+
     // Connect to the PostgreSQL database
     let (client, connection) = tokio_postgres::connect(&database_url, NoTls)
         .await
@@ -65,14 +59,13 @@ async fn main() -> std::io::Result<()> {
 
     let client_data = web::Data::new(client);
 
-
-    HttpServer::new(move || 
+    HttpServer::new(move || {
         App::new()
             .app_data(client_data.clone())
             .route("/noun", web::get().to(get_noun))
             .route("/verb", web::get().to(get_verb))
-    )
-        .bind("0.0.0.0:8081")?
-        .run()
-        .await
+    })
+    .bind("0.0.0.0:8081")?
+    .run()
+    .await
 }
